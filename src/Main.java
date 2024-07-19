4a25a04ae8443a5bf5b8e8ecf7bafcb0f26033e1
iggonicollas <iggonicollas@gmail.com>
2024-07-19 11:33:25 -0300

    Implementacao de ArrayList e trazendo informação de clientes cadastrados.

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        List<Cliente> clientes = new ArrayList<>();
 
         Cliente Iggon = new Cliente();
         Iggon.setNome("Iggo Nicollas");
+        clientes.add(Iggon);
 
-        Conta cc = new ContaCorrente(Iggon);
-        Conta cp = new ContaPoupanca(Iggon);
+            Conta cc1 = new ContaCorrente(Iggon);
+            Conta cp1 = new ContaPoupanca(Iggon);
 
-        cc.depositar(100);
+        Cliente Caiog = new Cliente();
+        Caiog.setNome("Caio Glauber");
+        clientes.add(Caiog);
 
+            Conta cc2 = new ContaCorrente(Caiog);
+            Conta cp2 = new ContaPoupanca(Caiog);
 
-        cc.imprimirExtrato();
-        cp.imprimirExtrato();
+        cc1.depositar(100);
+
+        System.out.println("Depósito executado com sucesso! \n");
+
+        cc1.imprimirExtrato();
+        cp1.imprimirExtrato();
 
         System.out.println("\nAguardando nova operação...\n");
 
-        cc.transferir(47, cp);
+        cc1.transferir(47, cp2);
 
         System.out.println("Transferencia executada com sucesso! \n");
 
-        cc.imprimirExtrato();
-        cp.imprimirExtrato();
+        cc1.imprimirExtrato();
+        cp1.imprimirExtrato();
+
+        System.out.println("\nConta Destino Caio Glauber \n");
+
+        cc2.imprimirExtrato();
+        cp2.imprimirExtrato();
+
+        System.out.println("\nLista de clientes:");
+        for (Cliente cliente : clientes) {
+            System.out.println(cliente.getNome());
+        }
     }
 }
